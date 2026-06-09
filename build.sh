045f299df7af449ee3ae74ac903d1d3103478182
kpetigoflorentin7-droid <kpetigoflorentin7@gmail.com>
2026-06-09 06:59:22 +0000

    PostgreSQL + Evaluation model + admin complet

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@will.com', 'will2026')" | python manage.py shell
-echo "✅ Déploiement terminé"
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@will.com', 'Admin1234')" | python manage.py shell
